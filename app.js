13943af475bb40c6340621caf7d998db7b069afd
江远强 <oncwnuH4P-2sxX_kRfgkuSZI_UuY@git.weixin.qq.com>
2020-07-29 17:56:42 +0800

    queryclocl  YY-MM-DD HH-mm

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ var demandRouter = require('./routes/demand');
 
 var app = express();
 
-// app.all('*', function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//    res.header('Access-Control-Allow-Headers', 'Content-Type');
-//    res.header('Access-Control-Allow-Methods', '*');
-//    res.header('Content-Type', 'application/json;charset=utf-8');
-//   next();
-// });
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Headers', 'Content-Type');
+   res.header('Access-Control-Allow-Methods', '*');
+   res.header('Content-Type', 'application/json;charset=utf-8');
+  next();
+});
 
 var session = require('express-session')
 // view engine setup
